0aa8fc8ae357267f455131b89d025c7b89a02c0d
Krzysio13 <01171657@pw.edu.pl>
2024-11-04 10:28:41 +0100

    v1

--- ResourceManager.hpp
+++ ResourceManager.hpp
@@ -1,8 +1,38 @@
 #pragma once
 
 #include "Resource.hpp"
+#include <memory>
 
-class ResourceManager
-{
-    // Twoja implementacja tutaj
+class ResourceManager {
+private:
+    std::unique_ptr<Resource> resource;
+
+public:
+    ResourceManager() : resource(std::make_unique<Resource>()) {}
+
+    double get() {
+        return resource->get();
+    }
+
+    ResourceManager(const ResourceManager& other) 
+        : resource(std::make_unique<Resource>(*other.resource)) {}
+
+    ResourceManager& operator=(const ResourceManager& other) {
+        if (this != &other) {
+            resource = std::make_unique<Resource>(*other.resource);
+        }
+        return *this;
+    }
+
+    ResourceManager(ResourceManager&& other) noexcept 
+        : resource(std::move(other.resource)) {}
+
+    ResourceManager& operator=(ResourceManager&& other) noexcept {
+        if (this != &other) {
+            resource = std::move(other.resource);
+        }
+        return *this;
+    }
+
+    ~ResourceManager() = default;
 };
